f7a7e39c8f9e2015d5d6031212ee40220391b9a7
Christian-Leonel <christiandiazluna@gmail.com>
2022-04-05 18:25:22 -0300

    "Corrección nombres archivos"

--- DH-MercadoLiebre/app.js
+++ DH-MercadoLiebre/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "/public")))
 
 app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, '/views/home.html'))
+    res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
 app.get('/register.html', (req, res) =>{
